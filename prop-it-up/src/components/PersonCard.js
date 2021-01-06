7ac52aa0fe77ec78ffe0134885f97d3902033027
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-06 12:50:08 -0800

    Updated PersonCard class to express State through a constructure and created a Bday function that manipulates the age of the selected person

--- prop-it-up/src/components/PersonCard.js
+++ prop-it-up/src/components/PersonCard.js
@@ -2,13 +2,25 @@ import React, { Component } from "react";
 
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.a1,
+        };
+    }
+    
+    Bday = () =>  {
+        let newAge = this.state.age + 1;
+        this.setState({ age: newAge });
+    }
     render() {
         return(
             <div>
                 
                 <h1>{this.props.ln1}, {this.props.fn1}</h1>
-                <p>Age: {this.props.a1}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hc1}</p>
+                <button onClick={ this.Bday }>Birthday Button for {this.props.fn1} {this.props.ln1}</button>
                 
             </div>
         )
